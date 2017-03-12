db5712926af233d837d3858089bbc7e567da69d9
Huan LI <zixia@zixia.net>
2017-03-12 23:42:29 +0800

    sync: half work

---
+++ index.ts
@@ -0,0 +1,9 @@
+import { Db }     from './src/db'
+import { Hostie } from './src/hostie'
+import { Botie }  from './src/botie'
+
+export {
+  Db,
+  Hostie,
+  Botie,
+}
---
+++ src/hostie.ts
@@ -0,0 +1,3 @@
+export type Hostie = {
+  id: string
+}
