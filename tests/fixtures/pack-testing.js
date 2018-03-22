7d89f0ca6824b7d7c0c7e46ceed8cec434cccdfa
Huan LI <zixia@zixia.net>
2018-03-22 14:50:48 +0800

    fix pack testing

--- tests/fixtures/pack-testing.js
+++ tests/fixtures/pack-testing.js
@@ -14,8 +14,11 @@ async function main () {
   const localServer = new LocalServer()
   for await (const serverFixtures of localServer.fixtures()) {
     const db = new Db(serverFixtures.USER.token, serverFixtures.ENDPOINTS)
+    await db.open()
+
     const hostieStore = new HostieStore(db)
     await hostieStore.open()
+
     console.log(`Pack Testing v${VERSION} PASSED.`)
   }
   return 0
