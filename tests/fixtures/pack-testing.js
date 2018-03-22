56a0edcdf63a457e86fc461991bc0b848d0d088b
Huan LI <zixia@zixia.net>
2018-03-22 14:09:19 +0800

    add pack testing script in ci

---
+++ tests/fixtures/pack-testing.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+const {
+  LocalServer,
+}               = require('@chatie/graphql')
+const {
+  VERSION,
+  Db,
+  HostieStore,
+}               = require('@chatie/db')
+// }                  = require('../../')
+
+async function main () {
+  const localServer = new LocalServer()
+  for await (const serverFixtures of localServer.fixtures()) {
+    const db = new Db(serverFixtures.USER.token, serverFixtures.ENDPOINTS)
+    const hostieStore = new HostieStore(db)
+    await hostieStore.open()
+    console.log(`Pack Testing v${VERSION} PASSED.`)
+  }
+  return 0
+}
+
+main()
+.then(process.exit)
+.catch(e => {
+  console.error(e)
+  process.exit(1)
+})
