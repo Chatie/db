ca51d70b5484bef48a787a492dc193d731f6605a
Huan LI <zixia@zixia.net>
2018-04-11 14:54:18 +0800

    use @chatie/db directory to output ngpackagr

--- scripts/dist-dev.sh
+++ scripts/dist-dev.sh
@@ -4,6 +4,6 @@ set -e
 npm run lint
 npm run ng:packagr
 
-rm -fr dist/node_modules
+#rm -fr @chatie/db/node_modules
 
-cp -Rav dist/* ../app/node_modules/@chatie/db/
+cp -Rav @chatie/db/* ../app/node_modules/@chatie/db/
